9fcdd2fda2388a9e999bcc1d7767643fb0e3b807
zynick <zynick@foxmail.com>
2017-06-13 23:37:19 +0800

    发送端：fix [x]读取命令行的id从配置文件获取指定任务 [x]获取数据保存文件 [x]调用transfer_file发送文件

---
+++ get_data/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/6/13 下午7:55
+# @Author  : Bear Chao
+# @Site    : http://blog.nickzy.com
+# @File    : __init__.py.py
+# @Software: PyCharm
